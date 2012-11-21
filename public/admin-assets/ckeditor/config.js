265a1c8403823cdf98c038448072b110826ee85d
Rob Hammond <robbiehammond@gmail.com>
2012-11-21 09:32:50 +0000

    Changed from Raptor Editor to CKEditor
    Although relatively lightweight, Raptor's documentation is pretty bad;
    CKEditor seems a more flexible solution

---
+++ public/admin-assets/ckeditor/config.js
@@ -0,0 +1,28 @@
+﻿/**
+ * Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	config.language = 'en';
+	config.uiColor = '#F5F5F5';
+
+	config.toolbar = 'mojoPub';
+ 
+	config.toolbar_mojoPub =
+	[
+		{ name: 'document', items : [ 'Source' ] },
+		{ name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+		{ name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','Scayt' ] },
+		{ name: 'insert', items : [ 'Image','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'
+                 ,'Iframe' ] },
+                '/',
+		{ name: 'styles', items : [ 'Styles','Format' ] },
+		{ name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
+		{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote' ] },
+		{ name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+		{ name: 'tools', items : [ 'Maximize' ] }
+	];
+};
+
